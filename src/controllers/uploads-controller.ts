9898ac7d151a3e94340c44e199764f284da451c6
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-07-14 09:57:06 -0300

    feat(upload): add file validation

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,7 +1,41 @@
 import { Request, Response } from "express"
 
+import * as z from "zod"
+
+import uploadConfig from "../configs/upload-config"
+
 export class UploadsController {
   async create(request: Request, response: Response) {
-    return response.json(request.file)
+    try {
+      const fileSchema = z
+        .object({
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+              `Formato de imagem inválido, formatos aceitos: ${uploadConfig.ACCEPTED_IMAGE_TYPES}`,
+            ),
+          filename: z.string().nonempty(),
+          size: z
+            .number()
+            .refine(
+              (size) => size <= uploadConfig.MAX_FILE_SIZE,
+              `O arquivo excedeu o tamanho máximo de ${uploadConfig.MAX_SIZE_MB}MB`,
+            ),
+        })
+        .loose()
+
+      const file = fileSchema.parse(request.file)
+
+      return response.json(file)
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        if (request.file) {
+          console.log(request.file)
+        }
+      }
+
+      throw error
+    }
   }
 }
