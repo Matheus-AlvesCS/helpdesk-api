46b26d0c7188b2912eed2b0da12cd55d4545a821
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-07-16 11:46:16 -0300

    feat(ticket): add service association to ticket creation

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -3,29 +3,35 @@ import * as z from "zod"
 
 import { prisma } from "../database/prisma"
 import { AppError } from "../utils/app-error"
-
 export class TicketsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       title: z.string().trim().min(5),
       status: z.enum(["open", "in_progress", "closed"]).default("open"),
       technicianId: z.uuid(),
+      serviceId: z.uuid(),
     })
 
-    const { title, status, technicianId } = bodySchema.parse(request.body)
+    const { title, status, technicianId, serviceId } = bodySchema.parse(
+      request.body,
+    )
 
-    const existingTicket = await prisma.ticket.findFirst({
-      where: {
-        title,
-      },
-    })
-
-    const existingTechnician = await prisma.user.findUnique({
+    const existingTechnician = await prisma.user.findFirst({
       where: {
         id: technicianId,
         role: "technician",
       },
     })
+
+    const existingService = await prisma.service.findUnique({
+      where: {
+        id: serviceId,
+      },
+    })
+
+    if (!existingService) {
+      throw new AppError("Serviço inválido")
+    }
 
     if (!existingTechnician) {
       throw new AppError("Esse técnico não existe")
@@ -35,17 +41,25 @@ export class TicketsController {
       throw new AppError("Usuário não autenticado", 401)
     }
 
-    if (existingTicket?.clientId === request.user.user_id) {
-      throw new AppError("Você já criou um serviço com esse nome")
-    }
+    const ticket = await prisma.$transaction(async (tx) => {
+      const ticket = await tx.ticket.create({
+        data: {
+          title,
+          status,
+          clientId: request.user.user_id,
+          technicianId,
+        },
+      })
 
-    const ticket = await prisma.ticket.create({
-      data: {
-        title,
-        status,
-        clientId: request.user.user_id,
-        technicianId,
-      },
+      const service = await tx.ticketService.create({
+        data: {
+          ticketId: ticket.id,
+          serviceId: serviceId,
+          price: existingService.price,
+        },
+      })
+
+      return { ...ticket, serviceId: service.serviceId, price: service.price }
     })
 
     return response.status(201).json(ticket)
