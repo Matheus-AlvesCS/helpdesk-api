4bab5981cae5aa714fde297aa9863d42f4e88f0b
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-07-17 10:53:21 -0300

    feat(ticket): implement method to technician add more services

---
+++ src/controllers/tickets-services-controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express"
+import * as z from "zod"
+
+import { prisma } from "../database/prisma"
+import { AppError } from "../utils/app-error"
+
+export class TicketsServicesController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      serviceIds: z.uuid().array(),
+    })
+
+    const paramsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { serviceIds } = bodySchema.parse(request.body)
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const existingTicket = await prisma.ticket.findUnique({
+      where: { id },
+    })
+
+    const existingServices = await prisma.service.findMany({
+      where: {
+        OR: serviceIds.map((service) => {
+          return { id: service }
+        }),
+      },
+    })
+
+    if (existingServices.length < serviceIds.length) {
+      throw new AppError("Um dos serviços escolhidos é inválido")
+    }
+
+    if (!existingTicket) {
+      throw new AppError("Esse ticket não existe", 404)
+    }
+
+    if (existingTicket.technicianId !== request.user.user_id) {
+      throw new AppError("Sem permissão", 401)
+    }
+
+    const newServices = await prisma.$transaction(async (tx) => {
+      const services = existingServices.map(async (service) => {
+        return await tx.ticketService.create({
+          data: {
+            ticketId: id,
+            serviceId: service.id,
+            price: Number(service.price),
+          },
+        })
+      })
+
+      return Promise.all(services)
+    })
+
+    return response.status(201).json(newServices)
+  }
+}
--- src/routes/tickets-routes.ts
+++ src/routes/tickets-routes.ts
@@ -1,18 +1,25 @@
 import { Router } from "express"
 
 import { TicketsController } from "../controllers/tickets-controller"
+import { TicketsServicesController } from "../controllers/tickets-services-controller"
 
 import { ensureAuthenticated } from "../middlewares/ensure-authenticated"
 import { verifyAuthorization } from "../middlewares/verify-authorization"
 
 const ticketsRoutes = Router()
 const ticketsController = new TicketsController()
+const ticketsServicesController = new TicketsServicesController()
 
 ticketsRoutes.use(ensureAuthenticated)
 ticketsRoutes.post(
   "/",
   verifyAuthorization(["client"]),
   ticketsController.create,
+)
+ticketsRoutes.post(
+  "/:id/service",
+  verifyAuthorization(["technician"]),
+  ticketsServicesController.create,
 )
 
 export { ticketsRoutes }
