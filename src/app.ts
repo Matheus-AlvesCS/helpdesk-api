82a3f9a24454821875215c49728c2aecb53ffb64
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2026-07-30 08:15:06 -0300

    feat(app): implement cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 import { routes } from "./routes"
 
@@ -7,7 +8,9 @@ import { errorHandler } from "./middlewares/error-handler"
 import uploadConfig from "./configs/upload-config"
 
 const app = express()
+
 app.use(express.json())
+app.use(cors())
 
 app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
